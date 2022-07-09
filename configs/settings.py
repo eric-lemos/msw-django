5e7fb83ce5acea967863ea19103ca4f5fbbf05c8
Eric Lemos <eric.lemoss@gmail.com>
2022-07-09 00:19:21 -0300

    Update settings.py

--- configs/settings.py
+++ configs/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#j-d5b+7z))iu+gfev$&-@tilx9!axu46l!d1q=j_x+i=f4f7'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,15 +74,15 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'PASSWORD': 'Built1$saud',
-        'HOST': '192.168.0.25',
-        'USER': 'saud',
-        'NAME': 'msw',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'PASSWORD': '#',
+#        'HOST': '#',
+#        'USER': '#',
+#        'NAME': '#',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
